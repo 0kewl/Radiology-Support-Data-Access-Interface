5bc4bd50d2b673a8cb36ea54be6c42ac3ccde389
Michael Ciancibello <cianci10@yahoo.com>
2013-10-31 14:16:20 -0400

    Added saved searches link to navbar.

--- app/views/components/menu.blade.php
+++ app/views/components/menu.blade.php
@@ -4,7 +4,7 @@
 	    <a class="brand" href="{{route('home') }}">Radiology Support Data Access Interface</a>
 	    <ul class="nav">
 	        <li><a href="{{ route('home') }}">Search Page</a></li>
-	        <li><a href="#">Saved Cases</a></li>
+	        <li><a href="#">Saved Searches</a></li>
 			<li><a id="all-search" href="#">All Cases</a></li>
 	    </ul>
 	</div>
