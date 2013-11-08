c7e5cc47401f7c19958cc08047464dd82e535d55
Daniel Gonzalez <dgonzalez14@jcu.edu>
2013-11-07 22:27:25 -0500

    mods for hash searching

--- app/views/components/menu.blade.php
+++ app/views/components/menu.blade.php
@@ -7,7 +7,10 @@
 	        <li><a href="#">Saved Searches</a></li>
 			<li><a id="all-search" href="#">All Cases</a></li>
 	    </ul>
-		<input id="keyword" name="keyword" placeholder="Search Hash tags" class="keyword input-large additional-keyword" type="text" color="white" style="float:right;">
+		<div class="hash-search" style="margin-top:5px; float:right;">
+			<input id="keyword" name="keyword" class="keyword input-large additional-keyword" type="text" color="white" style="height:15px; margin-top:5px;">
+			<button id="hash-search" class="btn btn-small btn-inverse" type="button" style="margin-top:-5px;">Search Hash-Tags</button>
+		</div>
 	</div>
 </div>
 <!-- END Menu -->
