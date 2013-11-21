a47d20ada5a27c37503309242d888a12a7c36b8b
Daniel Gonzalez <dgonzalez14@jcu.edu>
2013-11-20 21:34:38 -0500

    menu spacing

--- app/views/components/menu.blade.php
+++ app/views/components/menu.blade.php
@@ -1,17 +1,17 @@
 <!-- Top menu bar with links -->
 <div class="navbar">
-	<div class="navbar-inner">
+	<div class="navbar-inner"  style="text-align:center;">
 	    <p class="brand">Radiology Support Data Access Interface</p>
 	    <ul class="nav">
-	        <li><a href="{{ route('home') }}">Search</a></li>
+	        <li><a href="{{ route('home') }}" style="margin-left: 30px; margin-right: 30px;">Search</a></li>
+			<li><a id="all-search" href="#" style="margin-right: 30px;">All Cases</a></li>
 	        <li><a href="{{route('get-bookmarks') }}">Bookmarks</a></li>
-			<li><a id="all-search" href="#">All Cases</a></li>
 	    </ul>
 		<div class="hashtag-search" style="margin-top:5px; float:right;">
-		<form id="search-hashtags" name="search-hashtags" action="{{ route('hashtag-results') }}" method="get">
-		    <input id="hashtag" name="hashtag" type="hidden" value="">
-		    <input id="hashtag-start" name="start" type="hidden" value="">
-		</form>
+			<form id="search-hashtags" name="search-hashtags" action="{{ route('hashtag-results') }}" method="get">
+				<input id="hashtag" name="hashtag" type="hidden" value="">
+				<input id="hashtag-start" name="start" type="hidden" value="">
+			</form>
 			<input id="hashtag-keyword" name="hashtag-keyword" class="keyword input-large additional-keyword" type="text" color="white" style="height:20px; margin-top:5px;">
 			<button id="hashtag-search-btn" class="btn btn-small btn-inverse" type="button" style="margin-top:-5px;">Search Hashtags</button>
 		</div>
