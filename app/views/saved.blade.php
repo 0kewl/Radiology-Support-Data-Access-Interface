db101c320bebbe0bebd1520286f175aa4e42ede1
Michael Ciancibello <cianci10@yahoo.com>
2013-12-03 11:52:05 -0500

    Added "no bookmarks found" indicator on bookmarks page.

--- app/views/saved.blade.php
+++ app/views/saved.blade.php
@@ -6,14 +6,26 @@
     <style>
 		.container {
 			width: 1322;
-			height: auto;
+			height: auto; 
 		}
+        .fancy {
+            text-align: center;
+            color: #eee;
+            font-size: 20px;
+            text-shadow: 2px 3px 3px #292929;
+            -webkit-text-stroke: 1px white;
+        }
     </style>
 <!-- Page wrapper -->
 <div class="container">
     @include('components/menu')
     <div class="row-fluid">
     	<div class="span12 shadow" style="height: 800px; overflow-y: hidden;">
+            @if (empty($bookmarks))
+                <div class="well">
+                    <p class="fancy">No Bookmarks Found</p>
+                </div>
+            @endif
             <ul>
             @foreach($bookmarks AS $bookmark)
                 <li><a href=" {{ URL::to('results') }}/{{ $bookmark[1] }}" style="color:#fff;">{{ $bookmark[0] }}</a> -- {{ $bookmark[2] }} (EST) -- 
