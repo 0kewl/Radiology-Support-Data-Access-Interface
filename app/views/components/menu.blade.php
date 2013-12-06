5581a1fa78e1c5059cd8191aeb3927b456b587f4
Thisdotgithub <kvandenhaute14@jcu.edu>
2013-12-06 15:56:13 -0500

    Fixed the header (menu) button-link area bugs.

--- app/views/components/menu.blade.php
+++ app/views/components/menu.blade.php
@@ -2,27 +2,36 @@
 <div class="navbar">
 	<div class="navbar-inner" style="text-align:center;">
 	    <h4>Radiology Support Data Access Interface</h4>
-	    <div style="margin-top:-25px;">
+		<div>
+	    <div>
 		    <ul class="nav">
 		        <li>
-		        	<a href="{{ route('home') }}">
-		        		<button id="search-btn" class="btn btn-small btn-inverse" type="button" style="margin-left: 25px; margin-top: 25px;">Search</button>
-		        	</a>
+					<div style="margin-left: 15px; margin-top: 30px;">
+						<a href="{{ route('home') }}">
+							<button id="search-btn" class="btn btn-small btn-inverse" type="button">Search</button>
+						</a>
+					</div>
 		        </li>
 				<li>
-					<a id="all-search" href="#">
-						<button id="all-cases-btn" class="btn btn-small btn-inverse" type="button" style="margin-left: 25px; margin-top: 25px;">All Cases</button>
-					</a>
+					<div style="margin-left: 35px; margin-top: 30px;">
+						<a id="all-search" href="#">
+							<button id="all-cases-btn" class="btn btn-small btn-inverse" type="button">All Cases</button>
+						</a>
+					</div>
 				</li>
 		        <li>
-		        	<a href="{{route('get-bookmarks') }}">
-		        		<button id="bookmarks-btn" class="btn btn-small btn-inverse" type="button" style="margin-left: 95px; margin-top: 25px;">Bookmarks</button>
-		        	</a>
+					<div style="margin-left: 115px; margin-top: 30px;">
+						<a href="{{route('get-bookmarks') }}">
+							<button id="bookmarks-btn" class="btn btn-small btn-inverse" type="button">Bookmarks</button>
+						</a>
+					</div>
 		        </li>
 		        <li>
-		        	<a href="{{route('get-saved-hashtags') }}">
-		        		<button id="hashtags-btn" class="btn btn-small btn-inverse" type="button" style="margin-left: 25px; margin-top: 25px;">Hashtags</button>
-		        	</a>
+					<div style="margin-left: 35px; margin-top: 30px;">
+						<a href="{{route('get-saved-hashtags') }}">
+							<button id="hashtags-btn" class="btn btn-small btn-inverse" type="button">Hashtags</button>
+						</a>
+					</div>
 		        </li>
 		    </ul>
 		</div>
@@ -35,6 +44,7 @@
 				<input id="hashtag-keyword" name="hashtag-keyword" class="keyword input-large additional-keyword" type="text" color="white" style="height:20px; margin-top:5px;">
 				<button id="hashtag-search-btn" class="btn btn-small btn-inverse" type="button" style="margin-top:-5px;">Search Hashtags</button>
 			</div>
+		</div>
 		</div>
 	</div>
 </div>
